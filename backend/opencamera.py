8ac59259910b595def57c2e6755f6e9edf4217b5
Victor <victorblay3@yahoo.es>
2022-05-22 21:50:46 +0200

    streamlit

--- backend/opencamera.py
+++ backend/opencamera.py
@@ -1,31 +1,30 @@
 import cv2
-from description import description_product
 from funciones import find_des, find_id
 from prepare_data import prepare_data
 
+def open_camera(path):
+    ruta_img = [i[0] for i in prepare_data(path)]
+    carpetas = [i[1] for i in prepare_data(path)]
 
-ruta_img = [i[0] for i in prepare_data("./queryimage")]
-carpetas = [i[1] for i in prepare_data("./queryimage")]
+    imagenes = []
+    for i in ruta_img:
+        cap = cv2.imread(i)
+        imagenes.append(cap)
 
-imagenes = []
-for i in ruta_img:
-    cap = cv2.imread(i)
-    imagenes.append(cap)
+    cap = cv2.VideoCapture(0)
 
-cap = cv2.VideoCapture(0)
+    deslist = find_des(imagenes)
 
-deslist = find_des(imagenes)
+    while True:
 
-while True:
+        success, img2 = cap.read()
+        img_original = img2.copy()
+        img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
-    success, img2 = cap.read()
-    img_original = img2.copy()
-    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+        id = find_id(img2, deslist)
+        if id != -1:
+            text_product = cv2.putText(img_original, carpetas[id], (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 1)
+            text_product
 
-    id = find_id(img2, deslist)
-    if id != -1:
-        text_product = cv2.putText(img_original, carpetas[id], (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 1)
-        text_product
-
-    cv2.imshow("img2", img_original)
-    cv2.waitKey(1)
+        cv2.imshow("img2", img_original)
+        cv2.waitKey(1)
--- streamlit/dashboard.py
+++ streamlit/dashboard.py
@@ -1,33 +1,51 @@
+import py_compile
+from cv2 import find4QuadCornerSubpix
 import streamlit as st
 from PIL import Image
-from backend import featuresimg
+import cv2
+
+page = st.sidebar.selectbox('Categoria gestionada ', 
+            ['Dolor',
+            'Higiene Bucodental'
+            'Insomnio',
+            'Invierno',
+            'Mosquitos',
+            'Piojos',
+            'Probioticos',
+            'Salud Ocular',
+            'Solares'])
+
+if page == 'Salud Ocular':
+    def main_loop():
+        st.title("OpenCV Demo App")
+        st.subheader("Esta app te permite saber el uso de los productos de parafarmacia que podras encontrar en nuestras oficinas de farmacia!")
+        st.text("We use OpenCV and Streamlit for this demo")
+
+        st.title("Webcam Live Feed")
+        run = st.checkbox('Run')
+        FRAME_WINDOW = st.image([])
+        camera = cv2.VideoCapture(0)
+
+        while run:
+            _, frame = camera.read()
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            FRAME_WINDOW.image(frame)
+        else:
+            st.write('Stopped')
+        
+
+        original_image = Image.open(image_file)
+        original_image = np.array(original_image)
+
+        processed_image = blur_image(original_image, blur_rate)
+        processed_image = brighten_image(processed_image, brightness_amount)
+
+        if apply_enhancement_filter:
+            processed_image = enhance_details(processed_image)
+
+        st.text("Original Image vs Processed Image")
+        st.image([original_image, processed_image])
 
 
-def main_loop():
-    st.title("OpenCV Demo App")
-    st.subheader("This app allows you to play with Image filters!")
-    st.text("We use OpenCV and Streamlit for this demo")
-
-    blur_rate = st.sidebar.slider("Blurring", min_value=0.5, max_value=3.5)
-    brightness_amount = st.sidebar.slider("Brightness", min_value=-50, max_value=50, value=0)
-    apply_enhancement_filter = st.sidebar.checkbox('Enhance Details')
-
-    image_file = st.file_uploader("Upload Your Image", type=['jpg', 'png', 'jpeg'])
-    if not image_file:
-        return None
-
-    original_image = Image.open(image_file)
-    original_image = np.array(original_image)
-
-    processed_image = blur_image(original_image, blur_rate)
-    processed_image = brighten_image(processed_image, brightness_amount)
-
-    if apply_enhancement_filter:
-        processed_image = enhance_details(processed_image)
-
-    st.text("Original Image vs Processed Image")
-    st.image([original_image, processed_image])
-
-
-if __name__ == '__main__':
-    main_loop()
+    if __name__ == '__main__':
+        main_loop()
